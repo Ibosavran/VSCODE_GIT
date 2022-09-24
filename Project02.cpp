13311c2b5eae0b4d49969cb94f786db4dd048346
Inosavran <savran@ktu.edu.tr>
2022-09-24 22:06:36 +0300

    init2

---
+++ Project02.cpp
@@ -0,0 +1,15 @@
+
+// main.cpp file
+#include <stdio.h>
+void main(){
+    char n ;
+    printf(" hello world\n");
+    n = 'A';
+    printf(" hello world %c\n", n );
+     
+} // end main
+
+
+
+
+
