eb25f1a5876e9b3e0b465003180a9437db5bc66f
Inosavran <savran@ktu.edu.tr>
2022-09-24 22:14:12 +0300

    init3

---
+++ Project03.cpp
@@ -0,0 +1,18 @@
+
+// main.cpp file
+#include <stdio.h>
+void main(){
+    char n ;
+    printf(" hello world\n");
+    n = 'A';
+    printf(" hello world %c\n", n );
+    printf(" hello world %c\n", n );
+    printf(" hello world %c\n", n );
+    printf(" hello world %c\n", n );
+     
+} // end main
+
+
+
+
+
